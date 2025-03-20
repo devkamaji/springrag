4a3145f7c148196b2133cd9f89ec9cd9f08ccd2a
Gabriel Nogueira <gabriel.nogueira21@hotmail.com>
2025-03-20 01:18:48 -0300

    feat: implement RAG service and update AgentController for new message handling

--- src/main/java/br/com/ganog/springrag/controller/AgentController.java
+++ src/main/java/br/com/ganog/springrag/controller/AgentController.java
@@ -1,69 +1,26 @@
 package br.com.ganog.springrag.controller;
 
 import br.com.ganog.springrag.controller.dto.MyQuestion;
-import br.com.ganog.springrag.controller.dto.MyStructuredTemplate;
-import br.com.ganog.springrag.controller.dto.MyStructuredTemplate.PromptDish;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.image.ImageModel;
-import dev.langchain4j.model.input.Prompt;
-import dev.langchain4j.model.input.structured.StructuredPromptProcessor;
-import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.model.openai.OpenAiImageModel;
+import br.com.ganog.springrag.rag.Assistant;
+import br.com.ganog.springrag.rag.RagService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.MalformedURLException;
-import java.util.List;
-
-import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_3_5_TURBO;
 
 @RestController
 @RequiredArgsConstructor
 public class AgentController {
 
-    @Value("${OPENAI_KEY}")
-    private String openAiKey;
+    private final RagService ragService;
+    private Assistant assistant;
 
-    @PostMapping("/answer")
-    public String chatOpenAi(@RequestBody MyQuestion myQuestion) {
+    @PostMapping("/rag")
+    public String messageRag(@RequestBody MyQuestion myQuestion) {
 
-        ChatLanguageModel customModel = new OpenAiChatModel.OpenAiChatModelBuilder()
-                .apiKey(openAiKey)
-                .modelName(GPT_3_5_TURBO)
-                .temperature(0.7)
-                .build();
-
-        return customModel.chat(myQuestion.question());
-    }
-
-    @PostMapping("/revenue")
-    public String makeARecipe() {
-        PromptDish promptDish = new PromptDish();
-        promptDish.dish = "Cozido";
-        promptDish.ingredients = List.of("batata", "cenoura", "carne", "cebola");
-
-        Prompt prompt = StructuredPromptProcessor.toPrompt(promptDish);
-
-        ChatLanguageModel customModel = new OpenAiChatModel.OpenAiChatModelBuilder()
-                .apiKey(openAiKey)
-                .modelName(GPT_3_5_TURBO)
-                .temperature(0.7)
-                .build();
-
-        return customModel.chat(prompt.text());
-    }
-
-    @PostMapping("/image")
-    public String generateImage(@RequestBody MyQuestion myQuestion) throws MalformedURLException {
-
-        ImageModel customImageModel = new OpenAiImageModel.OpenAiImageModelBuilder()
-                .apiKey(openAiKey)
-                .modelName("dall-e-2")
-                .build();
-
-        return customImageModel.generate(myQuestion.question()).content().url().toURL().toString();
+        if (assistant == null) {
+            assistant = ragService.configure();
+        }
+        return assistant.aswer(myQuestion.question());
     }
 }
--- src/main/java/br/com/ganog/springrag/rag/RagService.java
+++ src/main/java/br/com/ganog/springrag/rag/RagService.java
@@ -1,13 +1,28 @@
 package br.com.ganog.springrag.rag;
 
 import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.internal.Utils;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.spring.AiService;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.net.URISyntaxException;
-import java.nio.file.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static java.lang.String.format;
@@ -16,16 +31,38 @@ import static java.lang.String.format;
 @RequiredArgsConstructor
 public class RagService {
 
+    @Value("${OPENAI_KEY}")
+    private final String openAiKey;
+
     public Assistant configure() {
-        List<Document> documents = List.of();
-        return null;
+        List<Document> documents;
+
+        documents = FileSystemDocumentLoader.loadDocuments(toPath("/documents"), glob("*.txt"));
+
+        return AiServices.builder(Assistant.class)
+                .chatLanguageModel(OpenAiChatModel.builder()
+                        .apiKey(openAiKey)
+                        .build())
+                .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+                .contentRetriever(createContentRetriever(documents))
+                .build();
     }
 
-    public PathMatcher glob(String glob) {
-        return FileSystems.getDefault().getPathMatcher(format("glob:%s", glob));
+    public ContentRetriever createContentRetriever(List<Document> documents) {
+
+        InMemoryEmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+
+        // conversao de tokens do documento em vetores
+        EmbeddingStoreIngestor.ingest(documents, embeddingStore);
+
+        return EmbeddingStoreContentRetriever.from(embeddingStore);
     }
 
-    public Path path(String path) {
+    public DocumentParser glob(String glob) {
+        return (DocumentParser) FileSystems.getDefault().getPathMatcher(format("glob:%s", glob));
+    }
+
+    public Path toPath(String path) {
         try {
             final var fileUrl = Utils.class.getClassLoader().getResource(path);
             return Paths.get(fileUrl.toURI());
