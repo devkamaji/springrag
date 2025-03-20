3e5829ce4bc975d4accf65e6afce68c506048561
Gabriel Nogueira <gabriel.nogueira21@hotmail.com>
2025-03-20 13:17:17 -0300

    feat: add messageNoRag endpoint to AgentController for chat functionality

--- src/main/java/br/com/ganog/springrag/controller/AgentController.java
+++ src/main/java/br/com/ganog/springrag/controller/AgentController.java
@@ -3,10 +3,14 @@ package br.com.ganog.springrag.controller;
 import br.com.ganog.springrag.controller.dto.MyQuestion;
 import br.com.ganog.springrag.rag.Assistant;
 import br.com.ganog.springrag.rag.RagService;
+import dev.langchain4j.http.client.jdk.JdkHttpClientBuilder;
+import dev.langchain4j.model.ollama.OllamaChatModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +29,21 @@ public class AgentController {
         }
 
         return assistant.aswer(myQuestion.question());
+    }
+
+    @PostMapping("/norag")
+    public String messageNoRag(@RequestBody MyQuestion myQuestion) {
+
+        final var chat = OllamaChatModel.builder()
+                .httpClientBuilder(new JdkHttpClientBuilder())
+                .baseUrl("http://localhost:11434")
+                .logRequests(true)
+                .logResponses(true)
+                .modelName("llama3")
+                .timeout(Duration.ofSeconds(300))
+                .maxRetries(2)
+                .build();
+
+        return chat.chat(myQuestion.question());
     }
 }
